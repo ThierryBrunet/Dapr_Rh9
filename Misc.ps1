2812569922fb5c69e713e0eab7ac9c524d81a217
Thierry <Thierry.Brunet@outlook.com>
2021-11-21 16:12:42 +1000

    Redis Alpine Thierry

--- Misc.ps1
+++ Misc.ps1
@@ -1,7 +1,34 @@
 ﻿
+########################################
+#region -- Manage Docker as a non-root user
+
+# The Docker daemon binds to a Unix socket instead of a TCP port. By default that Unix socket is owned by
+# the user root and other users can only access it using sudo. The Docker daemon always runs as the root user.
+
+# If you don’t want to preface the docker command with sudo, create a Unix group called docker and add users
+# to it. When the Docker daemon starts, it creates a Unix socket accessible by members of the docker group.
+# To create the docker group and add your user:
+
+# Create the docker group.
+sudo groupadd docker
+
+# Add your user to the docker group.
+sudo usermod -aG docker $USER
+
+# Log out and log back in so that your group membership is re-evaluated.
+
+# If testing on a virtual machine, it may be necessary to restart the virtual machine for changes to take effect.
+# On a desktop Linux environment such as X Windows, log out of your session completely and then log back in.
+# On Linux, you can also run the following command to activate the changes to groups:
+newgrp docker
+
+# Verify that you can run docker commands without sudo.
+docker run hello-world
+
+#endregion
 
 ########################################
-#region Visual Studio Code on Linux
+#region -- Visual Studio Code on Linux
 
 # RHEL, Fedora, and CentOS based distributions#
 
@@ -28,7 +55,7 @@ sudo dnf install <file>.rpm
 #endregion
 
 ########################################
-#region install dotnet-sdk-6.0 on RHEL8
+#region -- install dotnet-sdk-6.0 on RHEL8
 
 # On RHEL 8 (for x64, arm64, and s390x), enter:
 dnf install dotnet-sdk-6.0
@@ -43,7 +70,7 @@ $ podman run --rm registry.redhat.io/ubi8/dotnet-60 dotnet --version
 #endregion
 
 ########################################
-#region GCC on Windows
+#region -- GCC on Windows
 https://www.mingw-w64.org/downloads/
 http://win-builds.org/doku.php
 http://win-builds.org/doku.php/download_and_installation_from_windows
@@ -60,7 +87,7 @@ https://code.visualstudio.com/docs/cpp/config-msvc
 #endregion
 
 ########################################
-#region GCC on Linux
+#region -- GCC on Linux
 
 # GCC, the GNU Compiler Collection
 https://gcc.gnu.org/
@@ -98,16 +125,14 @@ Hello World!
 
 #endregion
 
-
 ########################################
-#region RH repositiries
+#region -- RH repositiries
 rhel-9-for-x86_64-appstream-beta-rpms
 
 #endregion
 
-
 ########################################
-#region Linux Integration services
+#region -- Linux Integration services
 
 # Once Linux Integration services are deployed, virtual machines running Linux distributions can use features
 # like Live Migration, Jumbo Frames, VLAN Tagging and Trunking, support for Symmetric multiprocessing (SMP),
@@ -129,9 +154,8 @@ ndf install hyperv-daemons
 
 #endregion
 
-
 ########################################
-#region Subscription-manager RHSCL
+#region -- Subscription-manager RHSCL
 
 su -
 subscription-manager repos --list | egrep rhscl
@@ -142,9 +166,8 @@ subscription-manager repos --enable rhel-7-server-optional-rpms
 
 #endregion
 
-
 ########################################
-#region Build REDIS on Linux
+#region -- Build REDIS on Linux
 
 https://developer.redis.com/create/from-source/
 
@@ -171,9 +194,8 @@ make
 make test
 
 
-Running Redis
--------------
-
+# Running Redis in src
+# --------------------
 To run Redis with the default configuration, just type:
 
     % cd src
@@ -194,14 +216,10 @@ as options using the command line. Examples:
 All the options in redis.conf are also supported as options using the command
 line, with exactly the same name.
 
-Running Redis with TLS:
-------------------
 
-Please consult the [TLS.md](TLS.md) file for more information on
-how to use Redis with TLS.
 
-Playing with Redis
-------------------
+# Playing with Redis
+# ------------------
 
 You can use redis-cli to play with Redis. Start a redis-server instance,
 then in another terminal try the following:
@@ -224,8 +242,8 @@ You can find the list of all the available commands at https://redis.io/commands
 
 
 
-# INSTALL
-#--------
+# INSTALL on this Linux machine
+#-------------------------------
 sudo cp src/redis-server /usr/local/bin/
 sudo cp src/redis-cli /usr/local/bin/
 # Or just using
@@ -236,7 +254,258 @@ sudo make install
 % ./install_server.sh
 
 
+#endregion
+
+########################################
+#region -- Docker build REDIS-server on Alpine
+
+https://registry.hub.docker.com/_/redis
+https://github.com/docker-library/redis/blob/a04a6df0a45894e1a630db4e84e0c12c7bcf326a/6.2/alpine/Dockerfile
+
+docker build -t redis-server .
+docker run -d --name redis-server-thierry -p 6379:6379 	redis-server
+docker ps
+docker exec -ti redis-server-thierry redis-cli ping
+docker exec -ti redis-server-thierry redis-cli info
+
+docker rm redis-server-thierry --force
+
+redis bins are here:	/usr/local/bin/
+redis conf is here:
+redis conf is here:
+redis backup is here: /data/dump.rdb
+
+redis conf is here: # Warning: no config file specified, using the default config.
+# In order to specify a config file use redis-server /path/to/redis.conf
+
+
+
+# Notes:
+
+https://dl-cdn.alpinelinux.org/alpine/v3.14/main/x86_64/APKINDEX.tar.gz
+https://dl-cdn.alpinelinux.org/alpine/v3.14/community/x86_64/APKINDEX.tar.gz
+
+
+
+
 
 #endregion
 
+########################################
+#region -- Docker Build REDIS-cli on Alpine
+
+FROM alpine
+LABEL Author = "Thierry Brunet <Thierry.Brunet@outlook.com>"
+
+ARG REDIS_VERSION="6.2.6"
+ARG REDIS_DOWNLOAD_URL="http://download.redis.io/releases/redis-${REDIS_VERSION}.tar.gz"
+
+RUN apk update && apk upgrade \
+    && apk add --update --no-cache --virtual build-deps gcc make linux-headers musl-dev tar \
+    && wget -O redis.tar.gz "$REDIS_DOWNLOAD_URL" \
+    && mkdir -p /usr/src/redis \
+    && tar -xzf redis.tar.gz -C /usr/src/redis --strip-components=1 \
+    && rm redis.tar.gz \
+    && make -C /usr/src/redis install redis-cli /usr/bin \
+    && rm -r /usr/src/redis \
+    && apk del build-deps \
+    && rm -rf /var/cache/apk/*
+
+CMD  ["redis-cli"]
+
+
+
+
+# builds of the image
+docker build -t thierry/redis-cli . --build-arg REDIS_VERSION=6.2.6
+
+# For example the following command will ping to hostname with port 6379 and will delete container after finish:
+docker run --rm --name redis-cli -it thierry/redis-cli redis-cli -h hostname -p 6379 ping
+
+#endregion
+########################################
+#region -- Build REDIS docker image
+
+https://programmerlib.com/build-a-basic-docker-image-using-dockerfile/
+
+Create a dockerfile
+Now let me show you hot to create to build Redis image with dockerfile.
+
+Step 1: Preparation.
+Create a clean directory and download Redis source code.
+
+$ cd ~
+$ mkdir docker-redis
+$ cd docker-redis
+$ wget https://download.redis.io/releases/redis-6.2.6.tar.gz
+Step 2: Write Dockerfile
+# base image: centos7
+FROM centos:centos7
+
+# install dependencies to compile redis source code
+RUN ["yum" , "install" , "-y" ,"gcc","gcc-c++","net-tools","make"]
+
+# set workdir to /usr/local
+WORKDIR /usr/local
+
+# copy a file to workdir
+ADD redis-5.0.10.tar.gz .
+
+# reset workdir to compile redis
+WORKDIR /usr/local/redis-5.0.10/src
+
+# install redis
+RUN make && make install
+
+# reset dir where redis is installed
+WORKDIR /usr/local/redis-5.0.10
+
+# expose a port
+EXPOSE 6379
+
+# run redis after container starts
+CMD ["redis-server"]
+Step 3: Build image.
+Now we can build image using below command.
+
+docker image build -t redis_demo .
+In this command above, the -t parameter is used to specify the name of the image file, and a colon can also be used to specify the label. If not specified, the default label is latest. The last dot represents the path where the Dockerfile file is located. The above example is the current path.
+
+Now run docker image ls to check result.
+
+$ docker image ls
+
+
+#endregion
+
+
+########################################
+#region -- Transition from Docker CLI to the Podman CLI
+
+# brings with it a new concept of rootless containers. Whereby a docker user would
+# need elevated privileges to build and run a docker container, podman does not.
+# This makes podman images significantly safer to use.
+
+https://medium.com/technopanti/docker-vs-podman-c03359fabf77
+
+# One of Podman's greatest advantages is its complete CLI compatibility with Docker.
+# In fact, when building Podman, Docker users can adapt without any significant changes.
+# For example, you can use the alias command to create a docker alias for Podman:
+ssh rh9wroot
+podman --version
+# podman version 3.3.1
+docker
+yum -y install 'podman-docker'
+docker --version
+exit
+
+# Buildah is the builder of podman images. We can use buildah bud as a replacement
+# for docker build. The bud is an acronym for build using dockerfile.
+
+#endregion
+
+########################################
+#region -- Docker images for ASP.NET Core
+
+https://docs.microsoft.com/en-us/aspnet/core/host-and-deploy/docker/building-net-docker-images?view=aspnetcore-6.0
+
+# Download the sample by cloning the .NET Docker repository:
+git clone https://github.com/dotnet/dotnet-docker
+
+# Run the app locally
+# --------------------
+# Run the following command to build and run the app locally:
+cd ~/projects/dotnet-docker/samples/aspnetapp/aspnetapp
+dotnet run
+
+# Go to http://localhost:5000 in a browser to test the app.
+
+# Run in a Linux container
+# ------------------------
+# Navigate to the Dockerfile folder at dotnet-docker/samples/aspnetapp
+cd ~/projects/dotnet-docker/samples/aspnetapp
+
+# Run the following commands to build and run the sample in Docker:
+docker build -t aspnetapp .
+docker run -it --rm -p 5000:80 --name aspnetcore_sample aspnetapp
+
+#endregion
+
+########################################
+#region -- DotNet Docker
+https://github.com/dotnet/dotnet-docker
+https://github.com/dotnet/dotnet-docker/tree/main/samples
+
+# Build an image for Alpine, Debian or Ubuntu
+# .NET multi-platform tags result in Debian-based images, for Linux. For example, you will pull a Debian-based image if you use a simple version-based tag, such as 6.0, as opposed to a distro-specific tag like 6.0-alpine.
+
+# This sample includes Dockerfile examples that explicitly target Alpine, Debian and Ubuntu. Docker makes it easy to use alternate Dockerfiles by using the -f argument.
+
+https://github.com/dotnet/dotnet-docker/blob/main/samples/dotnetapp/README.md
+
+# The following example demonstrates targeting distros explicitly and also shows the size differences between the distros. Tags are added to the image name to differentiate the images.
+docker build --pull -t dotnetapp:debian -f Dockerfile.debian-x64 .
+docker build --pull -t dotnetapp:ubuntu -f Dockerfile.ubuntu-x64 .
+docker build --pull -t dotnetapp:alpine -f Dockerfile.alpine-x64 .
+
+# You can use docker images to see the images you've built and to compare file sizes:
+
+# % docker images dotnetapp
+REPOSITORY          TAG                 IMAGE ID            CREATED             SIZE
+dotnetapp           alpine              8933fb9821e8        8 seconds ago       87MB
+dotnetapp           ubuntu              373df08a06ec        25 seconds ago      187MB
+dotnetapp           debian              229dd121a96b        39 seconds ago      190MB
+dotnetapp           latest              303eabf97376        56 seconds ago      190MB
+
+# You can run any of the images you've just built with the following commands:
+docker run --rm dotnetapp
+docker run --rm dotnetapp:debian
+docker run --rm dotnetapp:ubuntu
+docker run --rm dotnetapp:alpine
+
+# If you want to double check the distro of an application, you can do that by configuring a different entrypoint when you run the image, as you see in the following example.
+
+% docker run --rm --entrypoint cat dotnetapp /etc/os-release
+	# PRETTY_NAME="Debian GNU/Linux 10 (buster)"
+	# NAME="Debian GNU/Linux"
+	# VERSION_ID="10"
+	# VERSION="10 (buster)"
+	# VERSION_CODENAME=buster
+	# ID=debian
+	# HOME_URL="https://www.debian.org/"
+	# SUPPORT_URL="https://www.debian.org/support"
+	# BUG_REPORT_URL="https://bugs.debian.org/"
+
+% docker run --rm --entrypoint cat dotnetapp:alpine /etc/os-release
+	# NAME="Alpine Linux"
+	# ID=alpine
+	# VERSION_ID=3.10.3
+	# PRETTY_NAME="Alpine Linux v3.10"
+	# HOME_URL="https://alpinelinux.org/"
+	# BUG_REPORT_URL="https://bugs.alpinelinux.org/"
+
+#endregion
+
+
+########################################
+#region -- Alpine Linux
+
+https://alpinelinux.org/releases/
+
+https://dl-cdn.alpinelinux.org/alpine/v3.14/releases/x86_64/alpine-standard-3.14.3-x86_64.iso
+https://dl-cdn.alpinelinux.org/alpine/v3.14/releases/x86_64/alpine-minirootfs-3.14.3-x86_64.tar.gz
+
+
+#endregion
+
+
+########################################
+#region -- CoreOS Fedora Linux
+
+https://getfedora.org/coreos?stream=stable
+
+https://console.cloud.google.com/marketplace/product/fedora-coreos-cloud/fedora-coreos-stable?pli=1
+
+
+#endregion
 
